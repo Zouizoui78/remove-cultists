6944581a1c481bdf77d2092dc82910e24a57ad0f
Alexandre Zouiten <alexandre.zouiten1@gmail.com>
2023-02-26 21:15:00 +0100

    first commit

--- plugin.cpp
+++ plugin.cpp
@@ -1,12 +1,104 @@
-SKSEPluginLoad(const SKSE::LoadInterface *skse) {
-    SKSE::Init(skse);
+#include <spdlog/sinks/basic_file_sink.h>
 
-    // Once all plugins and mods are loaded, then the ~ console is ready and can
-    // be printed to
-    SKSE::GetMessagingInterface()->RegisterListener([](SKSE::MessagingInterface::Message *message) {
-        if (message->type == SKSE::MessagingInterface::kDataLoaded)
-            RE::ConsoleLog::GetSingleton()->Print("Hello, world!");
-    });
+namespace logger = SKSE::log;
+
+void SetupLog() {
+    auto logsFolder = SKSE::log::log_directory();
+    if (!logsFolder) {
+        SKSE::stl::report_and_fail("SKSE log_directory not provided, logs disabled.");
+        return;
+    }
+    auto pluginName = SKSE::PluginDeclaration::GetSingleton()->GetName();
+    auto logFilePath = *logsFolder / std::format("{}.log", pluginName);
+    auto fileLoggerPtr = std::make_shared<spdlog::sinks::basic_file_sink_mt>(logFilePath.string(), true);
+    auto loggerPtr = std::make_shared<spdlog::logger>("log", std::move(fileLoggerPtr));
+    spdlog::set_default_logger(std::move(loggerPtr));
+    spdlog::set_level(spdlog::level::trace);
+    spdlog::flush_on(spdlog::level::info);
+}
+
+class EventHandler :
+    public RE::BSTEventSink<RE::MenuOpenCloseEvent> {
+    EventHandler() = default;
+    ~EventHandler() = default;
+    EventHandler(const EventHandler&) = delete;
+    EventHandler(EventHandler&&) = delete;
+    EventHandler& operator=(const EventHandler&) = delete;
+    EventHandler& operator=(EventHandler&&) = delete;
+
+public:
+    static EventHandler& GetSingleton() {
+        static EventHandler singleton;
+        return singleton;
+    }
+
+    RE::BSEventNotifyControl ProcessEvent(
+        const RE::MenuOpenCloseEvent* event,
+        RE::BSTEventSource<RE::MenuOpenCloseEvent>*) override
+    {
+        if (!event || event->opening || event->menuName != "Loading Menu") {
+            return RE::BSEventNotifyControl::kContinue;
+        }
+
+        auto player = RE::PlayerCharacter::GetSingleton();
+        if (!player) {
+            return RE::BSEventNotifyControl::kContinue;
+        }
+
+        auto location = player->GetCurrentLocation();
+        if (!location) {
+            return RE::BSEventNotifyControl::kContinue;
+        }
+
+        // check if we are in the arcanaeum
+        if (location->GetFormID() != 0x1eb79) {
+            return RE::BSEventNotifyControl::kContinue;
+        }
+
+        logger::info("{} is in the arcanaeum, looking for cultists", player->GetName());
+        auto forms = location->GetAllForms();
+        forms.second.get().LockForRead();
+        uint8_t count = 0;
+        for (const auto &[key, val] : *(forms.first)) {
+            RE::Actor *actor = val->As<RE::Actor>();
+            if (
+                actor &&
+                // !actor->IsDead() &&
+                actor != player &&
+                actor->Is3DLoaded() &&
+                actor->GetCurrentLocation() == location &&
+                strcmp(actor->GetName(), "Boethiah Cultist") == 0
+            ) {
+                // actor->KillImpl(player, 1e6, false, false);
+                actor->SetDelete(true);
+                count++;
+            }
+        }
+        forms.second.get().UnlockForRead();
+
+        if (count != 0) {
+            auto msg = std::format("Deleted {} cultists", count);
+            logger::info("{}", msg);
+            RE::DebugNotification(msg.c_str());
+        }
+
+        return RE::BSEventNotifyControl::kContinue;
+    }
+};
+
+SKSEPluginLoad(const SKSE::LoadInterface* skse) {
+    SKSE::Init(skse);
+    SetupLog();
+
+    auto *event_source = RE::ScriptEventSourceHolder::GetSingleton();
+    auto *ui_source = RE::UI::GetSingleton();
+
+    if (!event_source || !ui_source) {
+        return false;
+    }
+
+    auto &event_handler = EventHandler::GetSingleton();
+    ui_source->AddEventSink<RE::MenuOpenCloseEvent>(&event_handler);
 
     return true;
 }
